ed74cf7766be2367593502e699e08d7d44bfc38c
Nick Earl <nick.r.earl@gmail.com>
2022-06-25 23:39:56 -0400

    added and tested database queries for later use

--- server.js
+++ server.js
@@ -18,14 +18,40 @@ const db = mysql.createConnection(
     console.log('connected to the election database')
 )
 
-db.query(`SELECT * FROM candidates`, (err, rows)=>{
-    console.log(rows)
-})
+// db.query(`SELECT * FROM candidates`, (err, rows)=>{
+//     console.log(rows)
+// })
+
+// db.query(`SELECT * FROM candidates WHERE id=1`, (err, row)=>{
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(row)
+// })
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(result)
+// })
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?,?,?,?)`
+
+// const params = [1, 'Ronald', 'Firbank', 1]
+
+// db.query(sql, params, (err, result) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(result)
+// })
 
 
-app.use((req, res) =>{
-    res.status(404).end()
-})
+// app.use((req, res) =>{
+//     res.status(404).end()
+// })
 
 app.listen(PORT, () =>{
     console.log(`Server running on ${PORT}`)
