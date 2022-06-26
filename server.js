cffe5d6b7e18cc24bc629abb57706d734fb5ebbd
Nick Earl <nick.r.earl@gmail.com>
2022-06-25 23:26:54 -0400

    initialized server and linked mysql2

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mysql = require('mysql2')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
+    },
+    console.log('connected to the election database')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows)=>{
+    console.log(rows)
+})
+
+
+app.use((req, res) =>{
+    res.status(404).end()
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server running on ${PORT}`)
+})
